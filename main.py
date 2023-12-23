1e754430f98ee071d9fdf8b28e291771cab2d326
vvmanz <vvmanz23@gmail.com>
2023-12-23 18:28:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sqlite3
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
-
 @app.route('/api/flights')
 def api_flights():
     conn = sqlite3.connect('AirTickets.db')
@@ -12,43 +11,48 @@ def api_flights():
     departure_city = request.args.get('departure_city')
     destination_city = request.args.get('destination_city')
     departure_date_str = request.args.get('departure_date')
-    return_date_str = request.args.get('return_date')
+    business_trip_days = int(request.args.get('business_trip_days'))
 
     departure_date = datetime.strptime(departure_date_str, '%Y-%m-%d').strftime('%d.%m.%Y')
-    return_date = datetime.strptime(return_date_str, '%Y-%m-%d').strftime('%d.%m.%Y')
 
     cursor.execute("""
-           SELECT * FROM Flights WHERE Departure_city=? AND Destination_city=? AND Date_departure=? AND Date_destination=?
-       """, (departure_city, destination_city, departure_date, return_date))
-    flights = cursor.fetchall()
+        SELECT * FROM Flights WHERE Departure_city=? AND Destination_city=? AND Date_departure=?
+        """, (departure_city, destination_city, departure_date))
+    outbound_flights = cursor.fetchall()
 
-    if not flights:
-        cursor.execute("""
-               SELECT * FROM Flights WHERE Departure_city=? AND Destination_city=? AND Date_departure=?
-           """, (departure_city, destination_city, departure_date))
-        flights = cursor.fetchall()
-        if flights:
-            response_data = {'flights': flights, 'alternative_results': 1}
-        else:
-            three_days_before = (datetime.strptime(departure_date, '%d.%m.%Y') - timedelta(days=3)).strftime('%Y-%m-%d')
-            three_days_after = (datetime.strptime(departure_date, '%d.%m.%Y') + timedelta(days=3)).strftime('%Y-%m-%d')
-            three_days_after_str = datetime.strptime( three_days_after, '%Y-%m-%d').strftime('%d.%m.%Y')
-            three_days_before_str = datetime.strptime( three_days_before, '%Y-%m-%d').strftime('%d.%m.%Y')
-            print(three_days_before_str)
-            print(three_days_after_str)
-            print(departure_date)
-            cursor.execute("""
-                SELECT * FROM Flights 
-                WHERE Departure_city=? 
-                AND Destination_city=? 
-                AND Date_departure BETWEEN ? AND ?
-            """, (departure_city, destination_city, three_days_before_str, three_days_after_str))
-            flights = cursor.fetchall()
-            response_data = {'flights': flights, 'alternative_results': 2}
+    return_date = (datetime.strptime(departure_date, '%d.%m.%Y') + timedelta(days=business_trip_days)).strftime(
+        '%d.%m.%Y')
+    cursor.execute("""
+        SELECT * FROM Flights WHERE Departure_city=? AND Destination_city=? AND Date_departure=?
+        """, (destination_city, departure_city, return_date))
+    return_flights = cursor.fetchall()
+
+    if not outbound_flights or not return_flights:
+        response_data = {'flights': [], 'alternative_results': 2}  # Результаты не найдены
     else:
-        response_data = {'flights': flights, 'alternative_results': 0}
+        # Создание всех возможных комбинаций билетов туда и обратно с временем прилета и вылета
+        flight_combinations = []
+        for outbound_flight in outbound_flights:
+            for return_flight in return_flights:
+                total_price = outbound_flight[6] + return_flight[6]
+                waiting_days = (datetime.strptime(return_flight[3], '%d.%m.%Y') - datetime.strptime(outbound_flight[4],
+                                                                                                    '%d.%m.%Y')).days - 1
+                outbound_departure_time = outbound_flight[4]
+                outbound_arrival_time = outbound_flight[5]
+                return_departure_time = return_flight[8]
+                return_arrival_time = return_flight[9]
+                flight_combinations.append((outbound_flight, return_flight, total_price, waiting_days,
+                                            outbound_departure_time, outbound_arrival_time, return_departure_time,
+                                            return_arrival_time))
 
-    print(response_data)
+        # Сортировка комбинаций по стоимости и дням ожидания
+        sorted_combinations = sorted(flight_combinations, key=lambda x: (x[2], x[3]))
+
+        if not sorted_combinations:
+            response_data = {'flights': [], 'alternative_results': 1}  # Альтернативные билеты могут подойти, но не подходят по одному из критериев
+        else:
+            response_data = {'flights': sorted_combinations, 'alternative_results': 0}  # Результаты найдены
+
     conn.close()
     return jsonify(response_data)
 
